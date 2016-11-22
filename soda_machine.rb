f4e8e5bc0cc7f1721e0367c22f0454060d3a558b
Cllie Tomlinson <callielynne3@gmail.com>
2016-11-22 13:45:20 -0800

    Refactor with teacher

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,21 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
-    # A soda machine will be able to return the number of sodas it contains.
-    @sodas.length
-  end
+    @sodas.count 
+    # .length 
+    # .size
+  end 
 
   def find_soda(soda_brand)
-    # A soda machine will be able to find a single soda with a given brand.
-    @sodas.find { |soda| soda.brand == soda_brand }
-  end
+    @sodas.find { |soda| soda.brand == soda_brand } 
+    # .detect 
+  end 
 
   def sell(soda_brand)
-    # A soda machine will be able to sell a soda with a given brand. 
-   sell_soda = find_soda(soda_brand)
-   return nil if sell_soda == nil
-   # When a soda is sold, it should be removed from the soda machine and the price of the soda should be added to the  soda machine's cash.
-   @cash += 
-  end
+    sell_soda = find_soda(soda_brand)
+    return nil if sell_soda == nil
+    @cash += sell_soda.price
+    @sodas.delete(sell_soda)
+  end 
 
-end
+end 
